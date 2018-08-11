02de232884fba4c6c5929eb5f2e2f1e7260e538b
ZriStart <3158722367@qq.com>
2018-08-11 20:09:01 +0800

    commit components files

---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class NavHeader extends React.Component {
+    render() {
+        return (
+            <div>
+                header
+            </div>
+        );
+    }
+}
---
+++ src/components/tabBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TabBar } from 'antd-mobile'
+
+export default class FotTabBar extends React.Component {
+    render() {
+        return (
+            <div>
+                footer
+            </div>
+        );
+    }
+}
