e474b223799d7c32c0b7ef1583f2ebe7f26cbad3
ZriStart <3158722367@qq.com>
2018-08-11 19:56:51 +0800

    commit or files

---
+++ src/pages/recommend/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Card} from 'antd-mobile'
+
+export default class Recommend extends React.Component {
+    render() {
+        return (
+            <div>
+                <Card title='info'>
+                    content
+                </Card>
+            </div>
+        );
+    }
+}
