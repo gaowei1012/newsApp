46860b6f265c20d7a7da8d03db9eb1f525dd97b6
gmw-zjw <gaomingwei2017@163.com>
2018-08-12 20:21:49 +0800

    修改网络请求中的错误

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,17 +1,18 @@
 import axios from 'axios'
-import { Modal } from 'antd-mobile'
 
 export default class Axios {
 
+    /**
+     * 网络请求封装
+     * @param {传入参数} options 
+     */
     static ajax(options) {
-        //let baseURL = '/list/?tag=__all__&ac=wap&count=40&format=json_raw&as=A1255B74B740FE0'
-        
-
+    
+        // 返回一个promise
         return new Promise((resolve, reject) => {
             axios({
                 url: options.url || '',
                 method: 'get',
-                //baseURL: baseURL,
                 params: (options.data && options.data.params) || ''
             }).then((response) => {
                 if (response.status == '200') {
